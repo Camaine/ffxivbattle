dace7f480d6ba8a1942c09354da1acad2049e2ef
Juwon Kim <kang9290@gmail.com>
2022-01-13 23:54:39 +0900

    TEST : 문자열 암복호화 테스트

--- src/test/java/com/green/ffxivbattle/FfxivbattleApplicationTests.java
+++ src/test/java/com/green/ffxivbattle/FfxivbattleApplicationTests.java
@@ -1,5 +1,6 @@
 package com.green.ffxivbattle;
 
+import com.green.ffxivbattle.utils.AES256;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -9,5 +10,28 @@ class FfxivbattleApplicationTests {
     @Test
     void contextLoads() {
     }
+
+    @Test
+    void dataEncryptTest(){
+        AES256 aes256 = new AES256();
+        try {
+            System.out.println("ENCRYPT : " + aes256.encrypt("test"));
+        }catch (Exception e){
+            System.out.println("ENCRYPT : FAIL : " + e.getMessage());
+        }
+
+    }
+
+    @Test
+    void dataDecryptTest(){
+        AES256 aes256 = new AES256();
+        try {
+            System.out.println("DECRYPT : " + aes256.decrypt("6WBSAFH7mi7Xmu275NuLhQ=="));
+        }catch (Exception e){
+            System.out.println("DECRYPT : FAIL : " + e.getMessage());
+        }
+
+    }
+
 
 }
