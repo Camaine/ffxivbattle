b65be79981c265006314374eff70a2384e614f5b
Juwon Kim <kang9290@gmail.com>
2022-01-15 16:01:37 +0900

    FEAT : 캐릭터 스탯 정보 ENTITIY 생성

---
+++ src/main/java/com/green/ffxivbattle/entity/characterStat.java
@@ -0,0 +1,42 @@
+package com.green.ffxivbattle.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "TB_CHARACTER_INFO")
+public class characterStat {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "nickname", length = 20)
+    private String nickname;
+
+    @Column(name = "job", length = 10)
+    private String job;
+
+    @Column(name = "hp")
+    private int hp;
+
+    @Column(name = "critical")
+    private int critical;
+
+    @Column(name = "directhit")
+    private int directhit;
+
+    @Column(name = "determination")
+    private int determination;
+
+    @Column(name = "defense")
+    private int defense;
+
+}
